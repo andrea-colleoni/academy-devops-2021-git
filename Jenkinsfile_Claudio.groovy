379c73f70553c4e24d08d3314d20b0076c578188
Claudio <claudio.gasbarra@gmail.com>
2021-04-21 12:25:03 +0200

    commit codice finale

--- Jenkinsfile_Claudio.groovy
+++ Jenkinsfile_Claudio.groovy
@@ -1,12 +1,93 @@
+def getBuildDate() {
+    return new Date().format('yyyy-MM-dd HH:mm:ss')
+}
+
+pipeline {
+    agent any
+    /*
+    tools {
+        maven 'Maven 3.8.1'
+    }
+    */
+    stages {
+        stage('Inizio Pipeline') {
+            steps {
+                echo """Inizio della pipeline: 
+                - Build n. ${env.BUILD_NUMBER} 
+                - Data e ora: $BuildDate"""
+
+            writeFile encoding: 'UTF-8', file: 'info.md', text: """# Informazioni di build
+
+            - Job name: ${env.JOB_NAME}
+            - Build number: ${env.BUILD_NUMBER}
+            - Build date: $BuildDate"""
+            }
+        }
+        stage('Checkout da GIT') {
+            steps {
+                git branch: 'main', url: 'https://github.com/andrea-colleoni/academy-devops-2021-git.git'
+                bat 'dir'
+            }
+        }
+        stage('Maven Compile') {
+            steps {
+                withMaven(maven: 'Maven 3.8.1') {
+                    bat 'mvn compile -f primi-tests/pom.xml'
+                }
+            }
+        }
+        stage('Maven Test') {
+            steps {
+                withMaven(maven: 'Maven 3.8.1') {
+                    bat 'mvn test -f primi-tests/pom.xml -Dwebdriver.gecko.driver=/C:/Users/andre/Desktop/Temp/Corsi/D-Thinks/DevOps/geckodriver-v0.29.1-win64/geckodriver.exe'
+                }                
+            }
+        }
+        /*
+        stage('Fail!') {
+            steps {
+                bat 'pippo'
+            }
+        }
+        */
+    }
+    post {
+      success {
+        junit 'primi-tests/target/surefire-reports/*.xml'
+      }
+      failure {
+        emailext (
+            body: "La build numero ${env.BUILD_NUMBER}  del job ${env.JOB_NAME} è fallita.", 
+            subject: "Pipeline ${env.JOB_NAME} fallito", 
+            to: 'andrea@colleoni.info'
+        )
+      }      
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*  quello che facevo io all'inizio
 pipeline {
     agent any
 
-    /*
+    
     tools {
-        // Install the Maven version configured as "M3" and add it to the path.
-        maven "M3"
+        // Install the Maven version
+        maven "Maven 3.8.1"
     }
-    */
+    
 
     stages {
         stage('Inizio Pipeline') {
@@ -15,12 +96,15 @@ pipeline {
                 - Build n. ${env. BUILD_NUMBER}
                 - Data e ora: ${new Date().format('yyyy-MM-ddd HH:mm:ss')}"""
             }
-
-            stage('Checkout da GIT') {
-                steps {
-                    echo "Riga di prova"
-                }
-            }
         }
+
+        stage('Checkout da GIT') {
+            steps {
+                echo "Riga di prova"
+            }
+            
+        }
+        
     }
 }
+*/ 
